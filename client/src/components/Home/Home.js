0c841aea48e440549977163abd3029d8a04e8c44
Dev-Steven <stevenchung650@gmail.com>
2020-08-24 13:21:25 -0700

    home page updated

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './Home.scss';
 
 const Home = () => {
 	return (
 		<div className='home'>
 			<div className='home-image'>
-				<h1>Welcome to 96th Ave.</h1>
+				<h1>Welcome to 96th Ave. Coffee</h1>
+				<div className='shop-btn'>
+					<Link to='/coffees'>
+						<h3>Shop</h3>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
