dfea187d3321cfc43981de732fb52161c01ebd30
Dev-Steven <stevenchung650@gmail.com>
2020-08-26 11:30:51 -0700

    corrected if statement for cartReducer

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -1,27 +1,32 @@
 import coffees from '../apis/coffees';
 import { ADD_TO_CART } from '../actions/types';
-import { getCoffee } from '../actions/index';
 
 export default async (state = {}, action) => {
 	switch (action.type) {
 		case ADD_TO_CART:
-			console.log('In cartReducer');
 			// first search the coffees to find the specific coffee - addedItem
-			let addedCoffee = await coffees.get(`/coffees?id=${action.id}`);
+			let addedCoffee = await coffees.get(
+				`/coffees?id=${action.payload.id}`
+			);
 			// check if item id exists in added items
-			let existedItem = await coffees.get(`/cart?id=${action.id}`);
-			// let existedItem = addedItems.find(item => action.id === item.id);
-			console.log(addedCoffee.data);
-			console.log(existedItem.data);
+			let existedItem = await coffees.get(
+				`/cart?id=${action.payload.id}`
+			);
+
+			// ** Need to copy the whole object using ... for immutability
+
 			// if it does, update quantity and total price
-			if (existedItem.data.length > 0) {
-				console.log(state);
-				addedCoffee.quantity += 1;
-				// return {
-				//     ...state, total:
-				// }
+			if (
+				existedItem.data[0] &&
+				existedItem.data[0].id === action.payload.id
+			) {
+				console.log('item exists');
+				// take state (empty obj) and copy everything from responseData into state
+				// then add the addedCoffee into there
+			} else {
+				// otherwise, add new item to added items where quntity = 1 and add to total price
+				console.log('item not in cart');
 			}
-		// otherwise, add new item to added items where quntity = 1 and add to total price
 		default:
 			return state;
 	}
