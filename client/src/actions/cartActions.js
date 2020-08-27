3ac08babb021dcd2a8126f41e3a9f478bbd83a0b
Dev-Steven <stevenchung650@gmail.com>
2020-08-26 23:45:40 -0700

    added logic for an item already in the cart

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -2,7 +2,17 @@ import { ADD_TO_CART } from './types';
 import coffees from '../apis/coffees';
 
 export const addToCart = id => async dispatch => {
-	const response = await coffees.get('/cart');
-	const responseData = response.data;
-	dispatch({ type: ADD_TO_CART, payload: { id, responseData } });
+	const responseCart = await coffees.get('/cart');
+	const cart = responseCart.data;
+
+	let responseSelectedCoffee = await coffees.get(`/coffees?id=${id}`);
+	let selectedCoffee = responseSelectedCoffee.data[0];
+
+	let responseItemExist = await coffees.get(`/cart?id=${id}`);
+	let itemExist = responseItemExist.data[0];
+
+	dispatch({
+		type: ADD_TO_CART,
+		payload: { id, cart, selectedCoffee, itemExist },
+	});
 };
--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -5,41 +5,30 @@ import _ from 'lodash';
 export default async (state = {}, action) => {
 	switch (action.type) {
 		case ADD_TO_CART:
-			// first search the coffees to find the specific coffee - addedItem
-			let selectedCoffee = await coffees.get(
-				`/coffees?id=${action.payload.id}`
-			);
-			console.log('selectedCoffee: ', selectedCoffee.data[0]);
-
-			let cartArray = await coffees.get(`/cart?id=${action.payload.id}`);
-
-			let coffeeToAdd = _.cloneDeep(selectedCoffee.data[0]);
-			// check if item id exists in added items
-
-			let itemInCart = cartArray.data[0];
+			let coffeeToAdd = _.cloneDeep(action.payload.selectedCoffee);
 
 			// if item in cart, update quantity and total price
-			if (itemInCart && itemInCart.id === action.payload.id) {
+			// action.payload.itemExist &&
+			if (action.payload.itemExist.id === action.payload.id) {
 				console.log('item in cart');
-				// take state (empty obj) and copy everything from responseData into state
-				// then add the addedCoffee into there
-			}
-			// otherwise, add new item to cart
-			else {
-				console.log('item not in cart');
 
-				// create quantity property where quantity = 1
+				coffeeToAdd.quantity += 1;
+				let newTotal = coffeeToAdd.price + action.payload.cart[0].total;
+
+				return {
+					...state,
+					total: newTotal,
+				};
+			} else {
 				coffeeToAdd.quantity = 1;
-				console.log('coffeeToAdd: ', coffeeToAdd);
+				let newTotal = coffeeToAdd.price + action.payload.cart[0].total;
 
-				// add to total price
-				let newTotal =
-					coffeeToAdd.price + action.payload.responseData[0].total;
-				console.log('newTotal: ', newTotal);
-
-				// update db.json file
-				// total: newTotal
-				// add new object coffeeToAdd
+				// cant figure out how to update cart
+				return {
+					...state,
+					total: newTotal,
+					[action.payload.id]: { coffeeToAdd },
+				};
 			}
 		default:
 			return state;
