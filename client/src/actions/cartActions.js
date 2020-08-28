d118e032a036af85087a88adbb8438dfbefd6420
Dev-Steven <stevenchung650@gmail.com>
2020-08-28 16:11:08 -0700

    json-server doesnt allow post access to nested elements

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,18 +1,48 @@
 import { ADD_TO_CART } from './types';
 import coffees from '../apis/coffees';
+import _ from 'lodash';
 
 export const addToCart = id => async dispatch => {
 	const responseCart = await coffees.get('/cart');
 	const cart = responseCart.data;
 
-	let responseSelectedCoffee = await coffees.get(`/coffees?id=${id}`);
-	let selectedCoffee = responseSelectedCoffee.data[0];
+	const responseCoffees = await coffees.get('/coffees');
+	const selectedCoffee = responseCoffees.data.find(
+		coffee => coffee.id === id
+	);
 
-	let responseItemExist = await coffees.get(`/cart?id=${id}`);
-	let itemExist = responseItemExist.data[0];
+	let itemExist = cart.items.find(item => item.id === id);
+
+	let newVals = {};
+
+	if (itemExist) {
+		// item already in cart...
+		// increase quantity property by one
+		itemExist.quantity += 1;
+		// increase total
+		let newTotal = itemExist.price + cart.total;
+
+		newVals = { itemExist, total: newTotal };
+	} else {
+		// add item to cart...
+		// deep copy to add new property quantity
+		let coffeeToAdd = _.cloneDeep(selectedCoffee);
+		// create new quantity property
+		coffeeToAdd.quantity = 1;
+		// add to total
+		let newTotal = coffeeToAdd.price + cart.total;
+
+		newVals = { coffeeToAdd, total: newTotal };
+	}
+
+	// apparently with json-server you can only access top level properties
+	// stopping shopping cart feature.
+	const response = await coffees.post('/cart.items', { ...newVals });
+
+	console.log('hello?', response.data);
 
 	dispatch({
 		type: ADD_TO_CART,
-		payload: { id, cart, selectedCoffee, itemExist },
+		payload: response.data,
 	});
 };
--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -2,34 +2,15 @@ import coffees from '../apis/coffees';
 import { ADD_TO_CART } from '../actions/types';
 import _ from 'lodash';
 
-export default (state = {}, action) => {
+export default async (state = {}, action) => {
 	switch (action.type) {
 		case ADD_TO_CART:
-			let coffeeToAdd = _.cloneDeep(action.payload.selectedCoffee);
+			console.log('cartReducer');
+			return {
+				...state,
+				[action.payload.coffeeToAdd.id]: action.payload,
+			};
 
-			// if item in cart, update quantity and total price
-			// action.payload.itemExist &&
-			if (action.payload.itemExist.id === action.payload.id) {
-				console.log('item in cart');
-
-				coffeeToAdd.quantity += 1;
-				let newTotal = coffeeToAdd.price + action.payload.cart[0].total;
-
-				return {
-					...state,
-					total: newTotal,
-				};
-			} else {
-				coffeeToAdd.quantity = 1;
-				let newTotal = coffeeToAdd.price + action.payload.cart[0].total;
-
-				// cant figure out how to update cart
-				return {
-					...state,
-					total: newTotal,
-					[action.payload.id]: { coffeeToAdd },
-				};
-			}
 		default:
 			return state;
 	}
